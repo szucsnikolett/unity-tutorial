b5256b411f88ac5e733bf17ab778b373b40911fd
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-05-28 22:17:12 +0200

    add camera follow script

---
+++ Brackeys-Unity-tutorial/Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Player;
+    // stores 3 floats as a vector
+    /* instead of parenting which results a first person view and rotates on collision,
+     this solution smoothly follows the reference object from behind */
+    public Vector3 Offset = new Vector3(0,1,-5);
+    void Update()
+    {
+        transform.position = Player.position + Offset;
+    }
+}
