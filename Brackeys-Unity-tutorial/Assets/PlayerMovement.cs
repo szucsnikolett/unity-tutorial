a1d6e16129d8aea2b44732523342f323e3d6c574
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-05-28 11:54:17 +0200

    get player input and update movement

--- Brackeys-Unity-tutorial/Assets/PlayerMovement.cs
+++ Brackeys-Unity-tutorial/Assets/PlayerMovement.cs
@@ -4,6 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     // reference to Player's Rigidbody component, drag the instance in the editor
     public Rigidbody Rb;
+    public float ForwardForce = 800f;
+    public float SidewaysForce = 600;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,15 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         // add force on z axis, use fixedDeltaTime instead of deltaTime because of FixedUpdate
-        Rb.AddForce(0, 0, 2000*Time.fixedDeltaTime);
+        Rb.AddForce(0, 0, ForwardForce*Time.fixedDeltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            Rb.AddForce(SidewaysForce*Time.fixedDeltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            Rb.AddForce(-SidewaysForce*Time.fixedDeltaTime, 0, 0);
+        }
     }
 }
