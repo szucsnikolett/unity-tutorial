9afd2ee00bdfdce5abb20eb7c6a197013847714d
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-05-28 10:39:02 +0200

    add movement script component to palyer

---
+++ Brackeys-Unity-tutorial/Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // reference to Player's Rigidbody component, drag the instance in the editor
+    public Rigidbody Rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Started...");
+    }
+
+    // Use FixedUpdate instead of Update because of physics
+    void FixedUpdate()
+    {
+        // add force on z axis, use fixedDeltaTime instead of deltaTime because of FixedUpdate
+        Rb.AddForce(0, 0, 2000*Time.fixedDeltaTime);
+    }
+}
