e3f7bc98d80ca777e0a83fd3a934a14a8ae7c4eb
Szucs Nikolett <lexi.nikolett.szucs@gmail.com>
2020-05-28 22:39:57 +0200

    add player collision

---
+++ Brackeys-Unity-tutorial/Assets/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    // stops reference player's movement on collision
+    public void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+}
